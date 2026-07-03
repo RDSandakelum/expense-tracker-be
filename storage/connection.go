995b35cf7d0fdd96193226ea9a2764af2f1e560d
RDSandakelum <dinushka.sandakelumr@gmail.com>
2026-07-04 01:41:05 +0530

    connection string

--- storage/connection.go
+++ storage/connection.go
@@ -12,34 +12,9 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	// 1. Get database connection credentials from Environment Variables
-	dbHost := os.Getenv("DB_HOST")
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	dbPort := os.Getenv("DB_PORT")
-
-	// Fallback to local defaults if env variables are empty
-	if dbHost == "" {
-		dbHost = "localhost"
-	}
-	if dbUser == "" {
-		dbUser = "postgres"
-	}
-	if dbPassword == "" {
-		dbPassword = "1111"
-	}
-	if dbName == "" {
-		dbName = "expense-tracker"
-	}
-	if dbPort == "" {
-		dbPort = "5432"
-	}
 
 	// 2. Build the connection string (DSN)
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Colombo",
-		dbHost, dbUser, dbPassword, dbName, dbPort,
-	)
+	dsn := os.Getenv("DSN")
 
 	// 3. Open the connection
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
